ad9d8a5f2218c31710bda2cc39ab949ac88c0d89
Diogo Justen <diogojusten@gmail.com>
2017-03-23 11:36:35 -0300

    Removed debugs

--- src/config.h
+++ src/config.h
@@ -1,8 +1,6 @@
 #ifndef config_h
 #define config_h
 
-//#include "Arduino.h"
-
 #define DEVICE_CONF_ARRAY_LENGHT 70
 
 class config_t
@@ -10,10 +8,8 @@ class config_t
   public:
     config_t() {};
     int confirmation;
-    char broker[DEVICE_CONF_ARRAY_LENGHT]; //= "casa-granzotto.ddns.net"; //MQTT broker
-    char topicHumidity[DEVICE_CONF_ARRAY_LENGHT]; //= "home/bedroom/humidity"; //MQTT topic
-//    char mqttUser[DEVICE_CONF_ARRAY_LENGHT]; // = "osmc";
-//    char mqttPassword[DEVICE_CONF_ARRAY_LENGHT]; // = "password1234"; (actualy not this one)
+    char broker[DEVICE_CONF_ARRAY_LENGHT];
+    char topicHumidity[DEVICE_CONF_ARRAY_LENGHT]
     char wifiSsid[DEVICE_CONF_ARRAY_LENGHT];
     char wifiPass[DEVICE_CONF_ARRAY_LENGHT];
 };
