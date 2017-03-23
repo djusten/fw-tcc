ae85ac42e8cec9fa8a20c96f734c7d3591c64d38
Diogo Justen <diogojusten@gmail.com>
2017-03-23 11:34:14 -0300

    Reading and Writing in eeprom and connecting on WiFi

--- src/main.ino
+++ src/main.ino
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Diogo Justn. All rights reserved.
+ * Copyright (C) 2017 Diogo Justen. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,12 +15,81 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-void setup() {
+#include "config.h"
+#include <ESP8266WiFi.h>
+#include "EEPROMAnything.h"
+
+#define EEPROM_MAX_ADDRS 512
+config_t config;
+
+bool testWifi(void) {
+  int c = 0;
+  Serial.println("\nTesting WiFi...");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Wifi connected!");
+      return true;
+    }
+    delay(500);
+    Serial.print(WiFi.status());
+    c++;
+  }
+  Serial.println("\nConnect timed out, opening AP");
+  return false;
+}
+
+bool saveConfig(int confirmation, char *broker, char *topicHumidity,
+                char *wifiSsid, char *wifiPass)
+{
+  config.confirmation = confirmation;
+  strncpy(config.broker, broker, sizeof(config.broker));
+  strncpy(config.topicHumidity, topicHumidity, sizeof(config.topicHumidity));
+  strncpy(config.wifiSsid, wifiSsid, sizeof(config.wifiSsid));
+  strncpy(config.wifiPass, wifiPass, sizeof(config.wifiPass));
+  EEPROMWriteAnything(0, config);
+
+  return true;
+}
+
+bool loadConfig(void)
+{
+  EEPROMReadAnything(0, config);
+
+  Serial.print("Confirmation number is: ");
+  Serial.println(config.confirmation);
+  Serial.print("Brokeris : ");
+  Serial.println(config.broker);
+  Serial.print("topicHumidity is: ");
+  Serial.println(config.topicHumidity);
+  Serial.print("wifiSsid is: ");
+  Serial.println(config.wifiSsid);
+  Serial.print("wifiPass is: ");
+  Serial.println(config.wifiPass);
+
+  return true;
+}
+
+void setup()
+{
   Serial.begin(115200);
+
+  EEPROM.begin(EEPROM_MAX_ADDRS);
+
+  loadConfig();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.persistent(false);
+  WiFi.begin(config.wifiSsid, config.wifiPass);
+  if (testWifi()) {
+    Serial.println("Could connect");
+  } else {
+    Serial.println("Could NOT connect");
+  }
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Onion bugs =)");
-  delay(1000);
-}
+  delay(5000);
 
+}
